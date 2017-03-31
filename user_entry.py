2b99ac3c83b47fc9ed9d867cf9a59f25e5b03362
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-04-01 02:31:39 +0545

    Allow multiple entries

--- user_entry.py
+++ user_entry.py
@@ -1,15 +1,29 @@
+user_list = []
 
-def main():
-    user_list = []
 
+def run():
+    print()
     user = {}
-
     user['first_name'] = input('First Name: ')
     user['last_name'] = input('Last Lame: ')
     user['email'] = input('Email: ')
     user['address'] = input('Address: ')
 
     user_list.append(user)
+
+
+def main():
+    while True:
+        # Run the program in the loop.
+        run()
+        print()
+        
+        # Ask the user if he wants more entries.
+        should_continue = input('More Entries (Y/N)? ')
+
+        # If the answer is not 'Y', exit.
+        if should_continue.upper() != 'Y':
+            break
 
     print(user_list)
 
