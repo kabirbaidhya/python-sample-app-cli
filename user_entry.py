dccd3d8b96f22dfb09cbcea3d01d7ec33a9dac5d
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-04-01 02:24:31 +0545

    Add user_entry.py to ask for user information

---
+++ user_entry.py
@@ -0,0 +1,16 @@
+
+def main():
+    user_list = []
+
+    user = {}
+
+    user['first_name'] = input('First Name: ')
+    user['last_name'] = input('Last Lame: ')
+    user['email'] = input('Email: ')
+    user['address'] = input('Address: ')
+
+    user_list.append(user)
+
+    print(user_list)
+
+main()
